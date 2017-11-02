ba87b80826eb7c064e607fbf36d1dc4adabee2ac
Álvaro Câmara <camara.alvaro@gmail.com>
2017-11-02 00:29:59 +0000

    Inserir Primeiros arquivos
    Primeiro Commit do projeto

---
+++ ProdutosWeb.js
@@ -0,0 +1,14 @@
+var http = require('http'),
+    url = require('url');
+var server = http.createServer(function (request,response) {
+
+    if(request.url == "/produtos"){
+        response.end("<html><body><h1>Listando os Produtos</h1></body></html>");
+    }else{
+        response.end("<html><body><h1>Home</h1></body></html>");
+    }
+});
+
+server.listen(3000);
+
+console.log("servidor está rodando");
